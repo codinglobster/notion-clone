ec928bc32aef052e15e965c02df998d7a7609055
xiao.liu <liuxiaoabc@gmail.com>
2024-01-27 18:50:14 +0800

    update： nav bar

---
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -0,0 +1,5 @@
+import react from 'react';
+
+export default function Home() {
+    return <div>123</div>
+}
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -29,7 +29,7 @@ import { UserItem } from "./user-item";
 import { Item } from "./item";
 import { DocumentList } from "./document-list";
 import { TrashBox } from "./trash-box";
-// import { Navbar } from "./navbar";
+import { Navbar } from "./navbar";
 
 export const Navigation = () => {
   const router = useRouter();
@@ -204,7 +204,7 @@ export const Navigation = () => {
           isMobile && "left-0 w-full"
         )}
       >
-        {/* {!!params.documentId ? (
+        {!!params.documentId ? (
           <Navbar
             isCollapsed={isCollapsed}
             onResetWidth={resetWidth}
@@ -213,10 +213,7 @@ export const Navigation = () => {
           <nav className="bg-transparent px-3 py-2 w-full">
             {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground" />}
           </nav>
-        )} */}
-        <nav className="bg-transparent px-3 py-2 w-full">
-            {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground" />}
-          </nav>
+        )}
       </div>
     </>
   )
